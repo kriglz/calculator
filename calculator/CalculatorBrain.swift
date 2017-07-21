182cda0a225abc32f138153db218a8ca9c8ac18c
Kristina Gelzinyte <kriglz@Kristinas-MacBook-Pro.local>
2017-07-21 17:03:14 +0300

    Initial commit for repository.

---
+++ calculator/CalculatorBrain.swift
@@ -0,0 +1,198 @@
+//
+//  CalculatorBrain.swift
+//  calculator
+//
+//  Created by Kristina Gelzinyte on 5/23/17.
+//
+//
+
+import Foundation
+
+struct CalculatorBrain {
+
+//Description string made out of description array
+    var description: String {
+        get {
+            var entireString = ""
+            for element in descriptionArray {
+                entireString.append(element)
+            }
+            return entireString
+        }
+    }
+    private var descriptionArray:[String] = []
+    
+    private enum Operation{
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+        case clearAll
+    }
+    
+    private var lastOperation: LastOperation = .equals
+    private enum LastOperation{
+        case constant
+        case unaryOperation
+        case binaryOperation
+        case equals
+        case clearAll
+        case setOperand
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "sin": Operation.unaryOperation(sin),
+        "exp": Operation.unaryOperation(exp),
+        "1/x": Operation.unaryOperation({ 1 / $0 }),
+//        "x^2": Operation.unaryOperation({ $0 * $0 }),
+        "±": Operation.unaryOperation({ -$0}),
+        "×": Operation.binaryOperation({ $0 * $1}),
+        "÷": Operation.binaryOperation({ $0 / $1}),
+        "+": Operation.binaryOperation({ $0 + $1}),
+        "-": Operation.binaryOperation({ $0 - $1}),
+        "=": Operation.equals,
+        "AC": Operation.clearAll,
+    ]
+
+    private var accumulation: Double?
+    
+    
+    
+    var resultIsPending = false
+    
+//performOperations for ViewCOntroller
+    mutating func performOperation (_ symbol: String){
+        if let operation = operations[symbol]{
+            switch operation {
+            
+            case .constant(let value):
+                if lastOperation != .binaryOperation {
+                    clearAll()
+                }
+                accumulation = value
+                appendToArray(symbol)
+                lastOperation = .constant
+                
+            case .unaryOperation (let function):
+                if accumulation != nil {
+                    
+                    var wrapSymbol:String = ""
+                    switch symbol {
+                    case "1/x":
+                        wrapSymbol = "1/"
+                    case "±":
+                        if accumulation! > 0 {
+                            wrapSymbol = "-"
+                        } else {
+                            wrapSymbol = ""
+                        }
+                    default:
+                        wrapSymbol = symbol
+                    }
+                    
+                    accumulation = function(accumulation ?? 0)
+                    
+                    if symbol == "±" && lastOperation != .equals {
+                        descriptionArray.insert(wrapSymbol, at: descriptionArray.startIndex)
+                    } else {
+                        if lastOperation == .unaryOperation {
+                            performOperation("=")
+                        }
+                        unaryOperationWrapping(wrapSymbol)
+                    }
+                    lastOperation = .unaryOperation
+                }
+                
+            case .binaryOperation(let function):
+                if lastOperation == .binaryOperation {
+                    descriptionArray.removeLast(1)
+                }
+                if accumulation != nil {
+                    pendingBindingOperation = PerformBinaryOperation(function: function, firstOperand: accumulation!)
+                    resultIsPending = true
+                    appendToArray(symbol)
+                    lastOperation = .binaryOperation
+                }
+
+            case .equals():
+                performPendingBinaryOperation()
+                lastOperation = .equals
+                resultIsPending = false
+                
+            case .clearAll():
+                clearAll()
+                lastOperation = .clearAll
+            }
+        }
+    }
+    
+//set operand for ViewController
+    mutating func setOperand (_ operand: Double){
+        if lastOperation == .constant || lastOperation == .equals || lastOperation == .unaryOperation {
+            descriptionArray = []
+        }
+        accumulation = nil
+        accumulation = operand
+        if accumulation != nil {
+            appendToArray(String(accumulation!))
+        }
+        lastOperation = .setOperand
+    }
+    
+//data structure for BinaryOperartion calculation
+    private struct PerformBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform (with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+//perform BinaryOperation
+    private var pendingBindingOperation: PerformBinaryOperation?
+    private mutating func performPendingBinaryOperation() {
+        if pendingBindingOperation != nil && accumulation != nil {
+            accumulation = pendingBindingOperation!.perform(with: accumulation!)
+            pendingBindingOperation = nil
+        }
+    }
+
+//set result for ViewController
+    var result: Double? {
+        get {
+            return accumulation
+        }
+    }
+    
+//clearAll description array and reset all instances
+    mutating private func clearAll() {
+        accumulation = 0
+        descriptionArray = ["0"]
+        pendingBindingOperation = nil
+        resultIsPending = false
+        lastOperation = .clearAll
+    }
+
+//append to array new elements
+    mutating private func appendToArray(_ element: String) {
+        if lastOperation == .clearAll && resultIsPending == false {
+            descriptionArray.removeAll()
+        }
+        descriptionArray.append(element)
+    }
+    
+//wraping before unitaryOperation
+    mutating private func unaryOperationWrapping(_ wrapSymbol: String) {
+        if lastOperation == .equals {
+            descriptionArray.insert(wrapSymbol + "(", at: descriptionArray.startIndex)
+        } else {
+            descriptionArray.insert(wrapSymbol + "(", at: descriptionArray.index(before: descriptionArray.endIndex))
+        }
+        descriptionArray.append(")")
+    }
+}
---
+++ calculator/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Kristina Gelzinyte on 5/19/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var descriptionDisplay: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+//
+//typing numbers
+//
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {      
+            if !display.text!.contains(".") || digit != "." {
+                let textCurrentlyDisplayed = display.text!
+                display.text! = textCurrentlyDisplayed + digit
+            }
+        } else {
+            display.text! = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        } set {
+            display.text = String(newValue)
+        }
+    }
+    
+    
+//
+//using/doiong operartion
+//
+    
+    private var brain = CalculatorBrain()
+    @IBAction func mathematicalSymbol(_ sender: UIButton) {
+//set operand
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+//perform operation
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+            
+//adding elipses or equal sign to the description label
+            if brain.resultIsPending {
+                descriptionDisplay.text! = brain.description + "..."
+            } else {
+                if mathematicalSymbol != "AC" {
+                    descriptionDisplay.text! = brain.description + "="
+                } else {
+                    descriptionDisplay.text! = brain.description
+                }
+            }
+        }
+//get result
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+}
+
